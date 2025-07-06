25ee0258a7c2cefda5cdd53e35f5cb11d64dcbf3
Yoshitha Udayanga <yoshithaudayanga9@gmail.com>
2025-07-06 17:08:47 +0530

    up

--- frontend/src/view/Home.jsx
+++ frontend/src/view/Home.jsx
@@ -507,7 +507,7 @@ const Home = () => {
           <div className="absolute bottom-0 left-0 w-full h-40 bg-gradient-to-t from-black/70 to-transparent"></div>
         </div>
 
-        <div className="absolute inset-0 bg-black/60 backdrop-blur-sm z-0" />
+        <div className="absolute inset-0 bg-black/60  z-0" />
 
         <div className="relative z-10 flex flex-col md:flex-row justify-center items-center h-full px-6 md:px-20 text-white">
           <div
