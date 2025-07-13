1a95ffe72f969f68229e7cfb306a0709c35912c8
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-07-13 18:14:05 +0530

    added

--- frontend/src/view/Policies/AccessEquityPolicy.jsx
+++ frontend/src/view/Policies/AccessEquityPolicy.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AccessEquityPolicy = () => {
   return (
     <>
-      <h1 className='text-black'>Access Equity Policy</h1>
+      <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 animate-pulse drop-shadow-lg mb-6'>Access Equity Policy</h1>
     </>
   )
 }
--- frontend/src/view/Policies/StudentInformationHandbook.jsx
+++ frontend/src/view/Policies/StudentInformationHandbook.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const StudentInformationHandbook = () => {
     return (
         <>
-            <h1 className='text-black'>Student Information Handbook</h1>
+            <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 animate-pulse drop-shadow-lg mb-6'>Student Information Handbook</h1>
         </>
     )
 }
