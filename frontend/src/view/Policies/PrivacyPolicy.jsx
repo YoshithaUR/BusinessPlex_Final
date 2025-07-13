789dfc068219bd4ce56a53baa94ca18af63faff4
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-07-13 19:14:19 +0530

    fixed the polices

--- frontend/src/view/Policies/PrivacyPolicy.jsx
+++ frontend/src/view/Policies/PrivacyPolicy.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PrivacyPolicy = () => {
     return (
         <>
-            <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 animate-pulse drop-shadow-lg mb-6'>Privacy Policy</h1>
+            <h1 className='p-20 text-black'>Privacy Policy</h1>
         </>
     )
 }
