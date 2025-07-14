346afc7c5569e190aca5c935224baa9e068ae0cd
Yoshitha Udayanga <yoshithaudayanga9@gmail.com>
2025-07-14 13:16:46 +0530

    up

--- frontend/src/components/layouts/RootLayout.jsx
+++ frontend/src/components/layouts/RootLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
+import MainFooter from './footer/MainFooter'
 
 const RootLayout = () => {
     return (
         <>
             <Header />
             <Outlet />
-            {/* //add footer here <Footer/> */}
+            <MainFooter />
         </>
     )
 }
