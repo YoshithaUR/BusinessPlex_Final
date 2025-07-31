4ebac314ce70efd67d2ea9e2060535a2021b8371
Yoshitha Udayanga <yoshithaudayanga9@gmail.com>
2025-07-31 11:51:53 +0530

    fb update edit

--- frontend/src/view/Home.jsx
+++ frontend/src/view/Home.jsx
@@ -1049,19 +1049,20 @@ const Home = () => {
 
                 <div className="w-full max-w-md mx-auto shadow-lg rounded-xl overflow-hidden hover:shadow-2xl transition-shadow duration-300">
                   <div
-                    dangerouslySetInnerHTML={{
-                      __html: `
-                      <iframe 
-                        src="https://www.facebook.com/share/16yoXUzBX7/"
-                        width="340"
-                        height="500"
-                        style="border:none;overflow:hidden;border-radius:12px;"
-                        scrolling="no"
-                        frameborder="0"
-                        allowfullscreen="true"
-                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                      ></iframe>
-                      `,
+                   dangerouslySetInnerHTML={{
+    __html: `
+      <iframe 
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F16yoXUzBX7&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
+        width="340"
+        height="500"
+        style="border:none;overflow:hidden;border-radius:12px;"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      ></iframe>
+    `,
+
                     }}
                   />
                 </div>
