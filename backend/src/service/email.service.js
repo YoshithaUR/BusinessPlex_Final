4b3eac05e3bae696c00c9bbdad2656348b1f8baa
Thushara_thilan <142692846+LUC1f3R-0@users.noreply.github.com>
2025-08-11 15:48:50 +0530

    fixed

--- backend/src/service/email.service.js
+++ backend/src/service/email.service.js
@@ -3,12 +3,23 @@ import 'dotenv/config'
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false, // true for 465, false for other ports
+    port: parseInt(process.env.SMTP_PORT),
+    secure: process.env.NODE_ENV !== 'production', // true for 465, false for other ports
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
   });
 
-  export default transporter;
+// SMTP verification function
+export const verifySMTP = () => {
+  transporter.verify((err, success) => {
+    if (err) {
+      console.error("SMTP Error:", err);
+    } else {
+      console.log("SMTP is ready:", success);
+    }
+  });
+};
+
+export default transporter;
